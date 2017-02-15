f94863b07f5a1d55914ba9cfccb897438bdb2bfd
olsen601 <tedssmith2556@gmail.com>
2017-02-15 17:58:41 -0600

    add message

--- src/com/RyanOlsen/Main.java
+++ src/com/RyanOlsen/Main.java
@@ -3,6 +3,6 @@ package com.RyanOlsen;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I'm A Dinosaur");// write your code here
+        System.out.println("I'm A Dinosaur");// he's a dinosaur
     }
 }
